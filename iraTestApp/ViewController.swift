9f16b5f8b0fb7f7dba7297da51353196191e2aaa
Test <Tesz>
2023-03-06 11:33:15 +0100

    Button check

--- iraTestApp/ViewController.swift
+++ iraTestApp/ViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
  
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var helloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        helloWorldLabel.isHidden = true
+        buttonOne.layer.cornerRadius = 10
+        
     }
 
-
-    
-
+    @IBAction func pressButtonOne(_ sender: Any) {
+        
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            buttonOne.setTitle("Hide", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            buttonOne.setTitle("Show", for: .normal)
+        }
+        
+    }
 }
 
